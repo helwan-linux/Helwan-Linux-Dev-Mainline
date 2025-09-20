ac1ecce701cc4cc3370506143d28ec62472e6525
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-09-20 14:19:32 +0300

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="Helwan-Linux-stable"
-iso_label="Helwan-Linux-stable-v1.1"
+iso_name="Helwan-Linux-Neo"
+iso_label="Helwan-Linux-Neo-v1"
 iso_publisher="helwanlinux <helwanlinux@gmail.com>"
 iso_application="Helwan Linux Live/Rescue DVD"
 iso_version="v1.1"
